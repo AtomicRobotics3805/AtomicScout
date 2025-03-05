4389e4e54efbd9259184c8a96f759d024785aea5
rowan-mcalpin <87948250+rowan-mcalpin@users.noreply.github.com>
2025-03-05 10:23:04 -0800

    Collect data from form, SUPER primitive table creation, TODO clear form data on submit

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const stepTeleop = document.getElementById("step_teleop");
 const stepEndgame = document.getElementById("step_endgame");
 
 function switchToInfo() {
-    infoFieldset.className = "fieldset bg-base-200 border-base-300 rounded-box mx-auto mt-10 w-md border p-4"
+    infoFieldset.className = "fieldset bg-base-200 border-base-300 rounded-box mx-auto mt-10 w-sm border p-4"
     autoFieldset.className = "hidden";
     teleopFieldset.className = "hidden";
     endgameFieldset.className = "hidden";
@@ -20,7 +20,6 @@ function switchToInfo() {
     stepTeleop.className = "step"
     stepEndgame.className = "step"
 }
-
 
 function switchToTeleop() {
     infoFieldset.className = "hidden"
@@ -59,4 +58,109 @@ function switchToEndgame() {
     stepAuto.className = "step"
     stepTeleop.className = "step"
     stepEndgame.className = "step step-primary"
+}
+
+function createJson(
+    teamNumber,
+    matchNumber,
+    startSide,
+
+    autoSamplesScored,
+    autoSamplesDropped,
+    autoSpecimensScored,
+    autoSpecimensDropped,
+    autoParkPos,
+    autoNotes,
+
+    teleopSamplesScored,
+    teleopSamplesDropped,
+    teleopSpecimensScored,
+    teleopSpecimensDropped,
+    teleopNotes,
+    teleopParkPos,
+
+    penaltyPoints,
+    broke,
+    robotDesign,
+    otherNotes
+) {
+    return {
+        teamNumber: teamNumber,
+        matchNumber: matchNumber,
+        startSide: startSide,
+        autoSamplesScored: autoSamplesScored,
+        autoSamplesDropped: autoSamplesDropped,
+        autoSpecimensScored: autoSpecimensScored,
+        autoSpecimensDropped: autoSpecimensDropped,
+        autoParkPos: autoParkPos,
+        autoNotes: autoNotes,
+        teleopSamplesScored: teleopSamplesScored,
+        teleopSamplesDropped: teleopSamplesDropped,
+        teleopSpecimensScored: teleopSpecimensScored,
+        teleopSpecimensDropped: teleopSpecimensDropped,
+        teleopNotes: teleopNotes,
+        teleopParkPos: teleopParkPos,
+        penaltyPoints: penaltyPoints,
+        broke: broke,
+        robotDesign: robotDesign,
+        otherNotes: otherNotes
+    };
+}
+
+function getFormValues() {
+    console.log("Submitting");
+    let json = localStorage.getItem('json');
+
+    if (json == null) {
+        localStorage.setItem('json', JSON.stringify([]));
+        json = localStorage.getItem('json');
+    }
+
+    const newJson = createJson(
+        document.getElementById("teamNum").value,
+        document.getElementById("matchNum").value,
+        document.getElementById("startSide").value,
+        document.getElementById("autoSamplesScored").value,
+        document.getElementById("autoSamplesDropped").value,
+        document.getElementById("autoSpecimensScored").value,
+        document.getElementById("autoSpecimensDropped").value,
+        document.getElementById("autoParkLocation").value,
+        document.getElementById("autoNotes").value,
+        document.getElementById("teleopSamplesScored").value,
+        document.getElementById("teleopSamplesDropped").value,
+        document.getElementById("teleopSpecimensScored").value,
+        document.getElementById("teleopSpecimensDropped").value,
+        document.getElementById("teleopNotes").value,
+        document.getElementById("endgameParkLocation").value,
+        document.getElementById("penaltyPoints").value,
+        document.getElementById("wasBroken").checked,
+        document.getElementById("robotDesign").value,
+        document.getElementById("otherNotes").value
+    );
+
+    let finalJson = JSON.parse(json);
+    finalJson.push(newJson);
+    localStorage.setItem('json', JSON.stringify(finalJson));;
+}
+
+function reset() {
+    localStorage.clear()
+}
+
+function createTable() {
+    const data = JSON.parse(localStorage.getItem('json'));
+
+    document.write("<table border==\"1\"><tr>");
+    for (key in data[0]) {
+        document.write('<td>' + key + '</td>');
+    }
+    document.write("</tr>");
+    for (var i = 0; i < data.length; i++) {
+        document.write('<tr>');
+        for (key in data[i]) {
+            document.write('<td>' + data[i][key] + '</td>');
+        }
+        document.write('</tr>');
+    }
+    document.write("</table>");
 }
